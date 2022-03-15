300263bb817b02b6f85b9ea4ae9339b0eded2bd9
Evelyn Rodriguez <evelynr812@gmail.com>
2022-03-15 01:07:15 -0400

    added data layer

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-react-redux');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -5,6 +7,51 @@ const path = require('path');
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-const port = process.env.PORT || 3000;
+const Book = sequelize.define('book', {
+    id: {
+        type: Sequelize.UUID,
+        primaryKey: true, 
+        defaultValue: Sequelize.UUIDV4
+    }, 
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    checkedOut: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+});
 
+const Author = sequelize.define('author', {
+    name: {
+        type: Sequelize.STRING
+    }
+});
+
+Book.belongsTo(Author);
+Author.hasMany(Book);
+
+const setup = async() => {
+    try {
+        await sequelize.sync({ force: true });
+
+        const leguin = await Author.create({ name: 'Ursula K. LeGuin' });
+        const butler = await Author.create({ name: 'Octavia Butler' });
+        const dick = await Author.create({ name: 'Philip K. Dick' });
+
+        await Book.create({ title: 'Left Hand of Darkness', checkedOut: true, authorId: leguin.id });
+        await Book.create({ title: 'The Dispossesed', checkedOut: false, authorId: leguin.id });
+        await Book.create({ title: 'Kindred', checkedOut: false, authorId: butler.id });
+        await Book.create({ title: 'Parable of the Sower', checkedOut: false, authorId: butler.id });
+        await Book.create({ title: 'A Scanner Darkly', checkedOut: false, authorId: dick.id });
+        await Book.create({ title: 'Do Androids Dream of Electric Sheep?', checkedOut: true, authorId: dick.id });
+    }
+    catch(err) {
+        console.log(err);
+    }
+};
+const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+setup();
